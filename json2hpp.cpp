6cdcf8fee1c533d4afba5ddd745a8e36067ba114
Tim Kane <tim.kane@gmail.com>
2024-07-22 17:13:59 +1000

    Improved error handling

--- json2hpp.cpp
+++ json2hpp.cpp
@@ -5,23 +5,35 @@
 int main(int argc, char* argv[]) {
     if (argc != 4)
     {
-        printf("Usage: json2cpp <name> <input.json> <output.hpp>");
+        printf("Usage: json2cpp <name> <input.json> <output.hpp>\n");
         return -1;
     }
 
     std::string name = argv[1];
     std::filesystem::path input = argv[2];
     std::filesystem::path output = argv[3];
-    
+
+    if (!std::filesystem::exists(input))
+    {
+        printf("Error: Input file does not exist\n");
+        printf("%s\n", input.filename().c_str());
+    }
+
     input = std::filesystem::absolute(input);
     output = std::filesystem::absolute(output);
 
     std::ifstream in{ input };
-    std::ofstream out{ output };
 
     std::string json{ std::istreambuf_iterator<char>{ in }, std::istreambuf_iterator<char>{} };
 
     auto res = kaixo::json::parse(json);
-    if (res) out << kaixo::json2hpp::generate(name, res.value());
+    if (!res)
+    {
+        printf("Error: No output generated\n");
+        return -1;
+    }
+
+    std::ofstream out{ output };
+    out << kaixo::json2hpp::generate(name, res.value());
     return 0;
 }
